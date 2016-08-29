5c1a06b0902fd1b5512f5df8703d4a441cb9271d
scottming <xiaowan5219@gmail.com>
2016-08-30 06:47:51 +0800

    Finished word_cloud

--- word_cloud.py
+++ word_cloud.py
@@ -1,28 +1,49 @@
 #!/usr/bin/env python3
+
+"""word_cloud
+
+Usage:
+    ./word_cloud.py -i <input> -o <output>
+
+Examples:
+    ./word_cloud.py -i tst.csv -o output.png
+
+Options:
+    -i --input
+    -o --output
+
+
+"""
+
+from docopt import docopt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='word cloud 0.1')
+    print(arguments)
+
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('../out.csv')
+df = pd.read_csv('%s' % arguments['<input>'])
 df = df[['Word','Freq']]
 tuples = [tuple(x) for x in df.values]
 
 from os import path
 from wordcloud import WordCloud
 
-d = path.dirname('__file__')
 
 # Generate a word cloud image
 wordcloud = WordCloud().generate_from_frequencies(tuples)
 
 # Display the generated image:
-# the matplotlib way:
 import matplotlib.pyplot as plt
-plt.imshow(wordcloud)
-plt.axis("off")
 
 # take relative word frequencies into account, lower max_font_size
-wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate_from_frequencies(tuples)
+wordcloud = WordCloud(
+            max_font_size=60, relative_scaling=.5
+            ).generate_from_frequencies(tuples)
 plt.figure()
 plt.imshow(wordcloud)
 plt.axis("off")
+plt.savefig('%s' % arguments['<output>'], dpi=300)
 plt.show()
